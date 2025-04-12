b39165b72ded54600145e68530a771d59d5a73d6
mayank sharma <mayank.rc.177@gmail.com>
2025-04-13 01:51:06 +0530

    remove unused code

--- src/main/java/com/DIY/BackendDiyApplication.java
+++ src/main/java/com/DIY/BackendDiyApplication.java
@@ -1,6 +1,5 @@
 package com.DIY;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendDiyApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-		System.setProperty("gemini.model-name", dotenv.get("GEMINI_MODEL_NAME"));
 		SpringApplication.run(BackendDiyApplication.class, args);
 	}
 
